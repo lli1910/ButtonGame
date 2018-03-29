3cbe7d39784f0898be6198bdc2a4d254d0d778a7
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-03-29 09:20:50 -0400

    fix them timers!

--- ButtonGames/src/application/FastClicker.java
+++ ButtonGames/src/application/FastClicker.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
 import javafx.animation.AnimationTimer;
@@ -27,6 +28,8 @@ public class FastClicker extends Application
         extracted(args);
     }
 	public int count = 0;
+	private long timeStep;
+	private boolean scoring;
 
 	private static void extracted(String[] args) {
 		launch(args);
@@ -35,7 +38,7 @@ public class FastClicker extends Application
     @Override
     public void start(Stage primaryStage) 
     {
-    	label = new Label();
+    	Label newLabel = new Label();
         primaryStage.setTitle("Click-O-Mania");
         Button btn = new Button();
         btn.setText("Click me!");
@@ -43,12 +46,38 @@ public class FastClicker extends Application
         
             @Override
             public void handle(ActionEvent event) {
-            	
-            	count++;
+            	if(scoring) {
+            		count++;
+            	}
+            	else
+            	{
+            		count--;
+            	}
                 System.out.println(count);
             }
             
         });
+        
+        timeStep = System.nanoTime() + 1000000000L;
+        new AnimationTimer()
+        {
+        	private Labeled txt;
+
+			public void handle(long now) {
+        		if(now > timeStep)
+        		{
+        			timeStep = now + 1000000000L;
+        			scoring = !scoring;
+        		}
+        		if(!scoring) {
+        			btn.setText("Don't Click");
+        		}
+        		else {
+        			btn.setText("Click Me!");
+        		}
+        		txt.setText("Score:" + Integer.toString(count));
+        	}
+        }.start();
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
